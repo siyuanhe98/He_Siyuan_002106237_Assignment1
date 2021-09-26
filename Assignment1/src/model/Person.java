61a99eb8758f5124d76eb9c8eab141dee54d9a2c
Siyuan He <he.siyua@northeastern.edu>
2021-09-26 00:27:16 -0400

    first version

---
+++ Assignment1/src/model/Person.java
@@ -0,0 +1,181 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Person {
+        
+    private String name;
+    private String city;
+    private String dateOfBirth;
+    private int age;
+    private String phone;
+    private String fax;
+    private String email;
+    private String ssn;
+    private String medicalRecord;
+    private String healthPlan;
+    private String bankAccount;
+    private String license;
+    private String linkedIn;
+    private String Ip;
+    private String nuid;
+    private String vin;
+    private String uid;
+    private String licensePlate;
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+    
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+    
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+
+        
+    public String getNuid() {
+        return nuid;
+    }
+
+    public void setNuid(String nuid) {
+        this.nuid = nuid;
+    }
+        
+    public String getIp() {
+        return Ip;
+    }
+
+    public void setIp(String Ip) {
+        this.Ip = Ip;
+    }
+        
+                
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+        
+        
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecord() {
+        return medicalRecord;
+    }
+
+    public void setMedicalRecord(String medicalRecord) {
+        this.medicalRecord = medicalRecord;
+    }
+
+    public String getHealthPlan() {
+        return healthPlan;
+    }
+
+    public void setHealthPlan(String healthPlan) {
+        this.healthPlan = healthPlan;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }  
+}
